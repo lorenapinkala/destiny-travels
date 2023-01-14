d6430e27dc82d938b12b0b6af47495de877adb32
lorenapinkala <baeksumin77@gmail.com>
2023-01-14 18:09:36 -0300

    maquetado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { ContentComponent } from './content/content.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    ContentComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
